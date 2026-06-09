8417b1a1ea8fdb2fdc8b49dc7ab6f89ee26124b2
v2pro1990-proto <v2pro1990@gmail.com>
2026-06-09 18:15:35 +0700

    v1.2.2 Update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = [
 
 setup(
     name="dynamic_font",                 
-    version="1.0.0",                       
+    version="1.2.2",                       
     author="v2pro1990",                    
     description="Font rendering Extesion for Pygame and Pygame-CE",
     ext_modules=cythonize(extensions, compiler_directives={'language_level': "3"}),
